47d009491f6ce33841fccb5c7347f56765a0a0ee
SamAyam <47021470+SamAyam@users.noreply.github.com>
2019-02-07 17:19:43 -0700

    Add files via upload

---
+++ Puzzle_Box/Assets/Scripts/Unscrew.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unscrew : MonoBehaviour {
+    public GameObject screw;
+    public GameObject button;
+    public GameObject sdvrIcon;
+    public GameObject hiddenButton;
+    public AudioSource speaker;
+    public AudioClip turnSound;
+
+    public void Turn()
+    {
+        if (sdvrIcon.activeInHierarchy)
+        {
+            screw.SetActive(false);
+            button.SetActive(false);
+            hiddenButton.SetActive(true);
+            speaker.PlayOneShot(turnSound, 0.7F);
+        }
+    }
+}
